fd41e188e3fd6d89a713d08fae5c46ab2057f01a
islamshu12 <islamshu12@gmail.com>
2023-02-27 16:34:31 +0200

    edit

--- resources/views/dashboard/parts/aside.blade.php
+++ resources/views/dashboard/parts/aside.blade.php
@@ -27,6 +27,11 @@
                 <i class="fa fa-pencil"></i>
                 <span class="menu-title">الاسئلة </span></a>
         </li>
+        <li class="nav-item  ">
+            <a href="{{ route('partners.index') }}">
+                <i class="fa fa-user"></i>
+                <span class="menu-title">الشركاء </span></a>
+        </li>
         <li class="nav-item has-sub "><a href="#"><i class="la la-users"></i><span class="menu-title" data-i18n="nav.menu_levels.main"> الاعضاء</span></a>
             <ul class="menu-content" style="">
               <li class="is-shown"><a class="menu-item" href="{{ route('users.index') }}" data-i18n="nav.menu_levels.second_level"> جميع الاعضاء</a>
