cab3961b7c1f5c4d8422baac293edd727fd7b98a
islamshu12 <islamshu12@gmail.com>
2023-03-04 16:58:31 +0200

    edit

--- resources/views/dashboard/users/show.blade.php
+++ resources/views/dashboard/users/show.blade.php
@@ -27,9 +27,8 @@
                                         <div class="row">
                                             <div class="col-md-6 mt-2">
                                                 <label>الصورة</label>
-                                                <input  readonlytype="file" name="image" class="form-control image" required >
                                                 <div class="form-group">
-                                                    <img src="" style="width: 100px"
+                                                    <img src="{{ route('uploads/'.$user->image) }}" style="width: 100px"
                                                         class="img-thumbnail image-preview" alt="">
                                                 </div>
                                             </div>
@@ -38,22 +37,19 @@
                                         <div class="row">
                                             <div class="col-md-6 mt-2">
                                                 <label>الاسم  </label>
-                                                <input  readonlytype="string" value="{{ old('name') }}" name="name" class="form-control" required >
+                                                <input  readonlytype="string" value="{{ $user->name}}" name="name" class="form-control" required >
                                             </div>
                                             <div class="col-md-6 mt-2">
                                                 <label>البريد الاكتروني  </label>
-                                                <input  readonlytype="email" value="{{ old('email') }}" name="email" class="form-control" required >
+                                                <input  readonlytype="email" value="{{ $user->email }}" name="email" class="form-control" required >
                                             </div>
                                             <br>
                                             <div class="col-md-6 mt-2">
                                                 <label> رقم الهاتف  </label>
-                                                <input  readonlytype="text" name="phone" value="{{ old('phone') }}" class="form-control" required >
+                                                <input  readonlytype="text" name="phone" value="{{ $user->phone }}" class="form-control" required >
                                             </div>
                                             <br>
-                                            <div class="col-md-6 mt-2">
-                                                <label>كلمة المرور   </label>
-                                                <input  readonlytype="password" name="password" class="form-control" required >
-                                            </div>
+                                        
                                         </div>
                                         <div class="row">
                                             <div class="col-md-6 mt-2">
@@ -61,7 +57,7 @@
                                                 <select name="packege_id" class="form-control" required id="packege_id">
                                                     <option value="" selected disabled>اختر</option>
                                                     @foreach (App\Models\Package::get() as $item)
-                                                    <option value="{{ $item->id }}" @if(old('packege_id') == $item->id ) selected @endif>{{ $item->title }}</option>    
+                                                    <option value="{{ $item->id }}" @if($user->packege_id == $item->id ) selected @endif>{{ $item->title }}</option>    
                                                     @endforeach
                                                 </select>
                                             </div>
@@ -71,14 +67,14 @@
                                                 <label>هل يملك موقع الكتروني    </label>
                                                 <select name="have_website" class="form-control" required id="have_website">
                                                     <option value="" selected disabled>اختر</option>
-                                                    <option value="1" @if(old('have_website') == 1 ) selected @endif>نعم</option>
-                                                    <option value="0" @if(old('have_website') == 0 ) selected @endif>لا</option>
+                                                    <option value="1" @if($user->have_website == 1 ) selected @endif>نعم</option>
+                                                    <option value="0" @if($user->have_website== 0 ) selected @endif>لا</option>
                                                 </select>
                                             </div>
                                             <br>
-                                            <div class="col-md-6 mt-2 site_url" @if(old('have_website') != 1 ) selected  style="display: none" @endif >
+                                            <div class="col-md-6 mt-2 site_url" @if($user->have_website != 1 ) selected  style="display: none" @endif >
                                                 <label>رابط الموقع    </label>
-                                                <input  readonlytype="url" name="site_url" value="{{ old('site_url') }}" id="site_url" class="form-control"  >
+                                                <input  readonlytype="url" name="site_url" value="{{ $user->site_url }}" id="site_url" class="form-control"  >
                                             </div>
                                            
                                         {{-- <div class="row">
