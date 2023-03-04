7aaef14d4b6b4c896aaf26c7abac1b5b0a8ff4a7
islamshu12 <islamshu12@gmail.com>
2023-03-04 15:30:38 +0200

    dd

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -24,7 +24,7 @@ class UserController extends BaseController
         // return($request->question_id);
         $date = (json_decode($request->question_id));
         foreach ($date as $key => $q) {
-            return $q;
+            return 'dd';
             
             $ans = new UserAnswer();
             $ans->user_id = $user->id;
