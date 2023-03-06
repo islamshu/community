fd51a8832bcb015f0907bef04f2082cfea4c4d78
islamshu12 <islamshu12@gmail.com>
2023-03-06 10:58:22 +0200

    edit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -54,8 +54,8 @@ class UserController extends BaseController
         $user->save();
         
         // $res = new UserResource($user);
-        $date = (($request->question_id));
-        $date2 = (($request->answer_id));
+        $date = json_encode(($request->question_id));
+        $date2 = json_encode(($request->answer_id));
         foreach (json_decode(@$date , @$date2)  as $key => $q) {
             if($q == null ){
                 continue;
