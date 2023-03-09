4645d883736183ae498ec8585288dbd39cf21a03
islamshu12 <islamshu12@gmail.com>
2023-03-09 14:22:56 +0200

    edit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -198,7 +198,7 @@ class UserController extends BaseController
             $product['invoice_id'] = date('Ymd-His') . rand(10, 99);
             $product['invoice_description'] = "Order #{$product['invoice_id']} Bill";
             $product['return_url'] = route('success.payment_service', $user->id);
-            $product['cancel_url'] = route('cancel.payment_service');
+            $product['cancel_url'] = route('cancel.payment_servicet');
             $product['total'] = $order->all_price;
             $paypalModule = new ExpressCheckout;
             $res = $paypalModule->setExpressCheckout($product);
