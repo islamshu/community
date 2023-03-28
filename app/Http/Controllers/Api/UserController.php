1f22a01a38647a2a03c274d2e92effa4f5224866
islamshu12 <islamshu12@gmail.com>
2023-03-28 15:10:11 +0200

    edit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -62,22 +62,22 @@ class UserController extends BaseController
 
             $date = json_encode(($request->question_id));
             $date2 = json_encode(($request->answer_id));
-            foreach (json_decode(@$date, @$date2)  as $key => $q) {
-                if ($q == null) {
-                    continue;
-                }
+            // foreach (json_decode(@$date, @$date2)  as $key => $q) {
+            //     if ($q == null) {
+            //         continue;
+            //     }
 
 
-                $ans = new UserAnswer();
-                $ans->user_id = $user->id;
-                $ans->question = Quastion::find((int)json_decode($date)[$key])->title;
-                if (is_numeric(json_decode($date2)[$key])) {
-                    $ans->answer = Answer::find(json_decode($date2)[$key])->title;
-                } else {
-                    $ans->answer = json_decode($date2)[$key];
-                }
-                $ans->save();
-            }
+            //     $ans = new UserAnswer();
+            //     $ans->user_id = $user->id;
+            //     $ans->question = Quastion::find((int)json_decode($date)[$key])->title;
+            //     if (is_numeric(json_decode($date2)[$key])) {
+            //         $ans->answer = Answer::find(json_decode($date2)[$key])->title;
+            //     } else {
+            //         $ans->answer = json_decode($date2)[$key];
+            //     }
+            //     $ans->save();
+            // }
             DB::commit();
             $ress = new UserAuthResource($user);
             return $this->sendResponse($ress, 'تم التسجيل بنجاح   ');
