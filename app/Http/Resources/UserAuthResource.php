7124967eacf83a902f93ef51e050bea54a3abcca
islamshu12 <islamshu12@gmail.com>
2023-04-04 14:57:19 +0200

    d

--- app/Http/Resources/UserAuthResource.php
+++ app/Http/Resources/UserAuthResource.php
@@ -27,7 +27,7 @@ class UserAuthResource extends JsonResource
             'is_paid'=>$this->is_paid,
             'domains'=>$this->domains,
             'social' => new SocialResource($this->soical),
-            'video_profile'=>get_general_value('video_profile'),
+            'video_profile'=>asset('uploads/').get_general_value('video_profile'),
             'answer_questione' =>  AnsweResourse::collection($this->answer),
             'token' => $this->createToken('Personal Access Token')->accessToken,
         ];
