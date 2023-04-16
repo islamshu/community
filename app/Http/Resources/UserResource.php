f810c58ad87dc6f48f647c35f52ae9137698479e
islamshu12 <islamshu12@gmail.com>
2023-04-16 13:08:36 +0200

    edit

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -29,7 +29,7 @@ class UserResource extends JsonResource
             'domains'=>$this->domains,
             'create_at'=>$this->created_at->format('Y-m-d H:i:s'),
             'star_color'=>$this->get_color($this),
-            'last_meeting_show'=>@UserVideo::where('email',auth('api')->user()->email)->orderby('id','desc')->first()->date,
+            'last_meeting_show'=>@UserVideo::where('email',$this->email)->orderby('id','desc')->first()->date,
             'social' => new SocialResource($this->soical),
             'answer_questione' =>  AnsweResourse::collection($this->answer),
             'video_profile'=>asset('uploads/'.get_general_value('video_profile')),
