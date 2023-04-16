c268d8415c08cc6465552961f38a9b3964107097
islamshu12 <islamshu12@gmail.com>
2023-04-16 14:38:53 +0200

    edit

--- app/Http/Resources/DomiansResourse.php
+++ app/Http/Resources/DomiansResourse.php
@@ -15,6 +15,7 @@ class DomiansResourse extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'=>$this->id,
             'title'=>$this->title,
             ];
     }
