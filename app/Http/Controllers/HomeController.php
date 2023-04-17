a31977a116e5a2059701c36e19e77a95d5da2f84
islamshu12 <islamshu12@gmail.com>
2023-04-17 15:07:24 +0200

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,7 +36,7 @@ class HomeController extends Controller
             ];
             $user->notify(new GeneralNotification($date_send));
             $message =" نريد تنبيهك الى ان الاشتراك الخاص بك في مجتمعنا سينتهي في تاريخ    :  <br>"  . str_replace('00:00:00','',$threeDaysFromNow);
-            dd($message);
+            // dd($message);
             Mail::to($user->email)->send(new AlertSubscribe($user->name,$user->email, $threeDaysFromNow,$message));
         }
         return $users;
