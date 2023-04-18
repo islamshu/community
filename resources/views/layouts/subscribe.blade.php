312dc5e148bbdc6668bad296faf68a6a8e1cd398
islamshu12 <islamshu12@gmail.com>
2023-04-18 15:16:44 +0200

    edit

--- resources/views/layouts/subscribe.blade.php
+++ resources/views/layouts/subscribe.blade.php
@@ -5,102 +5,71 @@
     <title>Subscribe to Our Mailing List</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.3/dist/sweetalert2.min.css">
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.3/dist/sweetalert2.all.min.js"></script>
-<link rel="stylesheet" href="{{ asset('front_css.css') }}">
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.3/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('front_css.css') }}">
 
 
     <style>
-        body {
-            background-image: url('https://communityapp.arabicreators.com/assets/header-c80fc340.png');
-            background-size: cover;
-            background-position: center;
-            font-family: Arial, sans-serif;
+        .session-register {
+            padding: clamp(8.5rem, 1.591rem + 4.55vw, 5rem) clamp(0.625rem, -1.875rem + 12.5vw, 7.5rem);
+            background-image: url("../../assets/header.png");
+            max-height: 100vh;
         }
-        
-        form {
-            max-width: 500px;
-            margin: 50px auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+        .logo {
+            width: clamp(11.25rem, 6.705rem + 22.73vw, 23.75rem);
+            height: clamp(8.75rem, 6.477rem + 11.36vw, 15rem);
+            background-color: #08324b;
         }
-        
-        h1 {
-            text-align: center;
-            color: #333;
-        }
-        input[type="text"] {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 20px;
-            border-radius: 5px;
-            border: none;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-            font-size: 16px;
-        }
-        input[type="email"] {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 20px;
-            border-radius: 5px;
-            border: none;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-            font-size: 16px;
-        }
-        
-        button[type="submit"] {
-            background-color: #333;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background-color 0.2s;
-        }
-        
-        button[type="submit"]:hover {
-            background-color: #555;
-        }
-        
-        img {
-            display: block;
-            margin: 0 auto;
-            padding: 20px;
-            border-radius: 23px;
-            max-width: 200px;
-            height: auto;
-            background: #323a52;
-            margin-bottom: 20px;
-        }
-        .invalid-feedback{
-            color: red;
-            margin-bottom: 10px;
+
+        .form-wrapper {
+            width: clamp(21.25rem, 17.614rem + 18.18vw, 31.25rem);
+            margin-inline: auto;
+            /* From https://css.glass */
+            background: rgba(255, 255, 255, 0.2);
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(5px);
+            -webkit-backdrop-filter: blur(5px);
+            border: 1px solid rgba(255, 255, 255, 0.3);
         }
     </style>
 </head>
 
 <body>
-    <form id="submit-form" method="post">
-        <img src="https://communityapp.arabicreators.com/assets/logo-59046dfd.png" alt="Company Logo">
-        @php
-            $date = today()->format('Y-m-d');
-        @endphp
-        @csrf
-        <h1>الاشتراك في جلسة تاريخ <span style="color: red">{{ $date }}</span> </h1>
-        <input type="hidden" name="date" value="{{ $date }}">
-        <input type="text" name="name" placeholder="ادخل الاسم">
-        <div class="invalid-feedback">
+    <section class="session-register w-100">
+        <div class="container p-0">
+            <div class="form-wrapper p-3">
+
+                <form id="submit-form" method="post" class="w-100 m-auto">
+                    <div class="logo p-3 rounded-5 mb-5 m-auto">
+                        <img src="../../assets/logo.png" alt="" />
+                        @php
+                            $date = today()->format('Y-m-d');
+                        @endphp
+                        @csrf
+                        <h1>الاشتراك في جلسة تاريخ <span style="color: red">{{ $date }}</span> </h1>
+                        <input type="hidden" name="date" value="{{ $date }}">
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label fs-5 text-white">ادخل الإسم</label>
+                        <input type="text" class="form-control p-2 fs-5" id="name" name="email"
+                            placeholder="أدخل الإسم" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label fs-5 text-white">أدخل البريد
+                            الإلكتروني</label>
+                        <input type="email" class="form-control p-2 fs-5" id="email" name="email"
+                            placeholder="name@example.com" />
+                    </div>
+                    <button type="submit" class="btn btn-primary fs-5 m-auto d-block">
+                        حفظ
+                    </button>
+                </form>
+            </div>
         </div>
-        <input type="email" name="email" placeholder="ادخل البريد الاكتروني">
-        <div class="invalid-feedback">
-        </div>
-        <button type="submit">Subscribe</button>
-    </form>
+    </section>
 </body>
 <script>
     $("#submit-form").submit(function(event) {
@@ -131,21 +100,21 @@
                         icon: 'error',
                         title: message,
                     })
-                } else if(response.responseJSON.status =='er') {
+                } else if (response.responseJSON.status == 'er') {
                     Swal.fire({
                         icon: 'error',
                         title: response.responseJSON.error,
                     }).then((result) => {
-                    location.replace('https://communityapp.arabicreators.com/signIn');
-                });
+                        location.replace('https://communityapp.arabicreators.com/signIn');
+                    });
 
-                }else if(response.responseJSON.status == 'erere'){
+                } else if (response.responseJSON.status == 'erere') {
                     Swal.fire({
                         icon: 'error',
                         title: response.responseJSON.error,
                     }).then((result) => {
                         location.replace('https://communityapp.arabicreators.com/packages');
-                });
+                    });
 
                 }
                 console.log(response.responseJSON.status);
