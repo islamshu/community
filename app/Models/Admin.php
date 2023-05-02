108ca2b078525608ee41c0ef2406cb3dd5d7c001
islamshu12 <islamshu12@gmail.com>
2023-05-02 11:40:32 +0300

    EDIT

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
-use illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Authenticatable
 {
