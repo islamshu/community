8eacda52101aa8b187c71e43e80fd09964db3c30
islamshu12 <islamshu12@gmail.com>
2023-05-11 15:00:55 +0300

    edit

--- app/Http/Resources/UserAuthResource.php
+++ app/Http/Resources/UserAuthResource.php
@@ -40,8 +40,7 @@ class UserAuthResource extends JsonResource
         ];
     }
     function get_domains($data){
-        dd(json_decode($data->domains));
-        return DomiansResourse::collection(Domians::whereIn('id',json_decode($data->domains))->get());
+        return DomiansResourse::collection(Domians::whereIn('id',($data->domains))->get());
     }
     function affilite_url($data){
         if($data->ref_code == null){
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -46,8 +46,8 @@ class UserResource extends JsonResource
     //     }
     // }
     function get_domains($data){
-        dd(json_decode($data->domains));
-        return DomiansResourse::collection(Domians::whereIn('id',json_decode($data->domains))->get());
+        // dd(json_decode($data->domains));
+        return DomiansResourse::collection(Domians::whereIn('id',($data->domains))->get());
     }
     function affilite_url($data){
         if($data->ref_code == null){
