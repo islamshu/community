417adbba2e48c597d1dabde46a0b55381d7962dc
islamshu12 <islamshu12@gmail.com>
2023-05-16 16:21:56 +0300

    edit

--- resources/views/dashboard/balance/show.blade.php
+++ resources/views/dashboard/balance/show.blade.php
@@ -29,11 +29,10 @@
                                             @php
                                                 $array_type = ($balace->payment_detiles);
                                                 $replacedData = str_replace(['{', '}'], ['[', ']'], $array_type);
-                                                $bankArray = json_decode($replacedData);
 
 
                                             @endphp
-                                            {{ dd((($bankArray))) }}
+                                            {{ dd((($replacedData))) }}
                                             <div class="col-md-6 mt-2">
                                                 <div class="form-group">
                                                     <label for="username">طريقة استلام :</label>
