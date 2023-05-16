1d22b4756b3cce69a003f88fa1d05de5ef5a7407
islamshu12 <islamshu12@gmail.com>
2023-05-16 16:07:33 +0300

    edit

--- app/Http/Controllers/BalaceRequestController.php
+++ app/Http/Controllers/BalaceRequestController.php
@@ -9,7 +9,7 @@ class BalaceRequestController extends Controller
 {
     public function index(){
         $balace = BlalnceRequest::orderby('id','desc')->get();
-        return view('dashboard.balance.index')->with('balace',$balace);
+        return view('dashboard.balance.index')->with('balaces',$balace);
     }
     public function withdrow_request($id){
         $balace = BlalnceRequest::find($id);
