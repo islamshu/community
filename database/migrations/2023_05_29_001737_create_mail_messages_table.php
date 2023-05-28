76387218e3014f5f4df210a3cbb4790da98af652
islamshu12 <islamshu12@gmail.com>
2023-05-29 00:19:27 +0300

    edit

---
+++ database/migrations/2023_05_29_001737_create_mail_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail_messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->mediumText('title');
+            $table->text('message');
+            $table->foreign('user_id')->references('id')->on('users')->onCascade('delete');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail_messages');
+    }
+};
