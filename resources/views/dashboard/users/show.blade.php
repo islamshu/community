ab03f5657fcc99c737b7e93337fea874ea70f506
islamshu12 <islamshu12@gmail.com>
2023-05-30 14:43:26 +0300

    edit

--- resources/views/dashboard/users/show.blade.php
+++ resources/views/dashboard/users/show.blade.php
@@ -240,14 +240,13 @@
         });
     </script>
      <script>
-        var chartData = @json($chartData);
         var chartData2 = @json($chartData2);
 
-        var ctx = document.getElementById('columnChart2').getContext('2d');
-        new Chart(ctx, {
+        var ctx2 = document.getElementById('columnChart2').getContext('2d');
+        new Chart(ctx2, {
             type: 'bar',
             data: {
-                labels: chartData.labels,
+                labels: chartData2.labels,
                 datasets: chartData2.datasets
             },
             options: {
