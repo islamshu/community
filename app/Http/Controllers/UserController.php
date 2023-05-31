e5194312ac42c3c7a35c8efd0d2ac65e016800c7
islamshu12 <islamshu12@gmail.com>
2023-05-31 14:55:40 +0300

    edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -163,11 +163,11 @@ class UserController extends Controller
             $messagecode =new  MailMessage();
             $messagecode->user_id = $user->id;
             $messagecode->title = 'تم رفض قبولك التسويق بالعمولة';
-            $messagecode->message = ': للاسف تم رفضك كمسوق في العمولة للاسباب التالية ' .'</br>'.$request->message;
+            $messagecode->message = ': للاسف تم رفضك كمسوق في العمولة للاسباب التالية '.$request->message;
             $messagecode->save();
             $mess=[
                 'title'=>'تم رفض قبولك التسويق بالعمولة',
-                'message' =>  'للاسف تم رفضك كمسوق في العمولة للاسباب التالية ' .$request->message,
+                'message' =>  'للاسف تم رفضك كمسوق في العمولة للاسباب التالية '  .'</br>'.$request->message,
             ];
             Mail::to($user->email)->send(new MessageEmail($mess));
         }
