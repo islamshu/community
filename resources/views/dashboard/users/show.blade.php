f5ae22f101a4acc90979f07d5102e9bfc8b877ff
islamshu12 <islamshu12@gmail.com>
2023-06-26 15:25:45 +0300

    edit

--- resources/views/dashboard/users/show.blade.php
+++ resources/views/dashboard/users/show.blade.php
@@ -40,7 +40,7 @@
 
                                                     </div>
                                                     @foreach ($user->soical_new as $item)
-                                                       <a target="_blank"  href="{{ $item->url }}/{{ $item->user_name }}"><img width="30" height="30" src="{{asset('uploads/'.$item->name.'.svg')  }}"  alt=""></a> 
+                                                       <a target="_blank"  href="{{ $item->url }}/{{ $item->user_name }}"><img width="30" height="30" src="{{asset('socail/'.$item->name.'.svg')  }}"  alt=""></a> 
                                                     @endforeach
                                                 {{-- @if (@$user->soical->facebook != null)
                                                     <a target="_blacnk"
