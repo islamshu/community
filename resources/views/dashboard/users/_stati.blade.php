3a9fd1e6de3a38af857e28fa1c4c436ee6a50587
islamshu12 <islamshu12@gmail.com>
2023-06-26 22:29:16 +0300

    edit

--- resources/views/dashboard/users/_stati.blade.php
+++ resources/views/dashboard/users/_stati.blade.php
@@ -123,6 +123,7 @@
             <label> مجموع المشتركين   </label>
             <input  readonly type="text" name="phone" value="{{ $paid_user }}" class="form-control" required >
         </div>
+        المشتركين : 
         <table class="table">
         <tr>
           <th>#</th>
