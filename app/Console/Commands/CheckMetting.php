5a03acb08ae09c43357106f90dae4640610d7d89
islamshu12 <islamshu12@gmail.com>
2023-07-06 00:49:22 +0300

    edit

--- app/Console/Commands/CheckMetting.php
+++ app/Console/Commands/CheckMetting.php
@@ -47,9 +47,9 @@ class CheckMetting extends Command
             // Fetch users associated with the community based on relevant criteria
             $users = User::where('is_paid',1)->get();
 
-            foreach ($users as $user) {
-                Mail::to($user->email)->send(new ReminderEmail($meetingDate,$community->id,$user->name));
-            }
+            // foreach ($users as $user) {
+            //     Mail::to($user->email)->send(new ReminderEmail($meetingDate,$community->id,$user->name));
+            // }
         }
         $expire = get_general_value('meeting_end');
         $communitiess = Community::where('meeting_end','<=',now())->get();
