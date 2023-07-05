63c546762117d45fde95a8968e760fcf646d5416
islamshu12 <islamshu12@gmail.com>
2023-07-05 14:57:11 +0300

    edit

--- resources/views/dashboard/users/chat.blade.php
+++ resources/views/dashboard/users/chat.blade.php
@@ -181,24 +181,7 @@
             </div>
           </div>
         </section>
-        <section class="chat-app-form">
-          <form class="chat-app-input d-flex">
-            <fieldset class="form-group position-relative has-icon-left col-10 m-0">
-              <div class="form-control-position">
-                <i class="icon-emoticon-smile"></i>
-              </div>
-              <input type="text" class="form-control" id="iconLeft4" placeholder="Type your message">
-              <div class="form-control-position control-position-right">
-                <i class="ft-image"></i>
-              </div>
-            </fieldset>
-            <fieldset class="form-group position-relative has-icon-left col-2 m-0">
-              <button type="button" class="btn btn-info"><i class="la la-paper-plane-o d-lg-none"></i>
-                <span class="d-none d-lg-block">Send</span>
-              </button>
-            </fieldset>
-          </form>
-        </section>
+       
       </div>
     </div>
   </div>
