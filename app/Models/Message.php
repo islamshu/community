c205804c27d549629eed0062e2c5f217ef9cd371
islamshu12 <islamshu12@gmail.com>
2023-07-05 13:58:53 +0300

    edit

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    /**
+     * Get the user that owns the Message
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
