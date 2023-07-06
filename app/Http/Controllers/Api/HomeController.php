3e085e3ebdce741c35535d2c30c5d387342cf83e
islamshu12 <islamshu12@gmail.com>
2023-07-06 17:38:58 +0300

    edit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -49,6 +49,7 @@ class HomeController extends BaseController
             }elseif($com->peroid_type == 'month'){
                 $startTime =  Carbon::parse($com->meeting_date)->addMonths($com->peroid_number);
             }
+            dd($startTime);
             $endTime = Carbon::parse($com->meeting_date)->addMinute($com->meeting_time);
             $emails = ['islamshu12@gmail.com'];
 
