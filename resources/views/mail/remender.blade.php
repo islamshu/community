b0f15f9462319abe5f4cbe09f9a9d7c574d76856
islamshu12 <islamshu12@gmail.com>
2023-07-06 17:27:52 +0300

    edit

--- resources/views/mail/remender.blade.php
+++ resources/views/mail/remender.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body style="text-align: center">
 
-    <h1>مرحبا {{ $name }} اسم الشخص</h1>
+    <h1>مرحبا {{ $name }}  </h1>
     <p>نود ان نذكركم بأنه سيتم عقد جلسة المجتمع مع الأعضاء بعد 3 ساعات من الان</p>
     <p> {{ $date }}<br> </p>
     <p>توقيت : مكة المكرمة | الرياض </p> <br>
@@ -24,7 +24,8 @@
         </p>
     <p>كونوا على الموعد لنحقق قصص نجاح واهدافنا سوياً </p>
     <a href="{{ $community->meeting_url }}" style="background: #1B7FED; color:white" class="btn btn-primary"> سجل دخولك </a>
-    <p>للاستفسار او التواصل من خلال الضغط هنا 
+    <p>للاستفسار او التواصل من خلال الضغط هنا  <br>
+        
         ( https://api.whatsapp.com/send/?phone=971506361956&text&type=phone_number&app_absent=0 ) 
         او مراسلتنا عبر : info@arabicreators.com</p>
 </body>
