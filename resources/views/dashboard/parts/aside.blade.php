9ccecc836d603bb04c57d2ef927c29d77194950a
islamshu12 <islamshu12@gmail.com>
2023-07-11 18:31:24 +0300

    edit

--- resources/views/dashboard/parts/aside.blade.php
+++ resources/views/dashboard/parts/aside.blade.php
@@ -67,10 +67,10 @@
                             data-i18n="nav.menu_levels.second_level"> جميع الاعضاء</a>
                     </li>
                     <li class="is-shown"><a class="menu-item" href="{{ route('users_paid.index') }}"
-                            data-i18n="nav.menu_levels.second_level"> جميع الاعضاء المشتركين</a>
+                            data-i18n="nav.menu_levels.second_level"> العضويات المدفوعة  </a>
                     </li>
                     <li class="is-shown"><a class="menu-item" href="{{ route('un_paid_user.index') }}"
-                            data-i18n="nav.menu_levels.second_level"> جميع الاعضاء الغير مشتركين</a>
+                            data-i18n="nav.menu_levels.second_level"> العضويات الغير مدفوعة</a>
                     </li>
 
                 </ul>
