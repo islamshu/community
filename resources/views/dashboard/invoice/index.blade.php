591401422b3f627934ceb71029f534df9b2974f3
islamshu12 <islamshu12@gmail.com>
2023-07-20 02:07:26 +0300

    edit

--- resources/views/dashboard/invoice/index.blade.php
+++ resources/views/dashboard/invoice/index.blade.php
@@ -102,7 +102,6 @@
                         <div class="form-group">
                           <label for="recipient-name" class="col-form-label">اسم صاحب الفاتورة:</label>
                           <select name="user_id" required id="mySelect">
-                            <option value="" selected >اختر صاحب الفاتورة</option>
                             @foreach ($users as $item)
                                 <option value="{{ $item->id }}">{{ $item->name }}</option>
                             @endforeach
