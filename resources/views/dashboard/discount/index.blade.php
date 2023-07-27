811be309f11841f3d53f06e804e311a88a34f827
islamshu12 <islamshu12@gmail.com>
2023-07-27 15:04:28 +0300

    edit

--- resources/views/dashboard/discount/index.blade.php
+++ resources/views/dashboard/discount/index.blade.php
@@ -57,7 +57,7 @@
                                                     <td>{{ $item->code }} </td>
                                                     <td>{{ $item->start_at }} </td>
                                                     <td>{{ $item->end_at }} </td>
-                                                    <td>{{ $item->discount_value  }}{{ $item->discount_type == 'rate' ? '%' : '$' }} </td>
+                                                    <td> <button class="fa fa-info">{{ $item->discount_value  }}{{ $item->discount_type == 'rate' ? '%' : '$' }} </button></td>
 
                                                     <td>
                                                         @can('update-discount')
