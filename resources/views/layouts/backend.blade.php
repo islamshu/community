87340052a710ded595606180fe7e8351826b97ec
islamshu12 <islamshu12@gmail.com>
2023-07-29 18:45:36 +0300

    edit

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -51,7 +51,6 @@
         href="{{ asset('backend/vendors/css/tables/datatable/datatables.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/switchery/0.8.2/switchery.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('backend/fonts/simple-line-icons/style.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/vendors/css/forms/selects/select2.min.css') }}">
     <style>
         .select2 {
             width: 100% !important;
