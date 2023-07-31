e24ea83628d860520d7d0f288c42fd33b8b18109
islamshu12 <islamshu12@gmail.com>
2023-07-31 23:54:14 +0300

    edit

--- resources/views/dashboard/claims/index.blade.php
+++ resources/views/dashboard/claims/index.blade.php
@@ -97,7 +97,7 @@
                     </div>
                 </div>
             </section>
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="exampleModal"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog  " role="document">
                   <div class="modal-content">
                     <div class="modal-header">
