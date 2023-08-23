875ac393d3714d3e4050b865259dd6c93d902a35
islamshu12 <islamshu12@gmail.com>
2023-08-24 00:49:44 +0300

    edit

--- app/Http/Resources/DiscountPackageResource.php
+++ app/Http/Resources/DiscountPackageResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Currency;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class DiscountPackageResource extends JsonResource
@@ -18,15 +19,25 @@ class DiscountPackageResource extends JsonResource
             // 'id'=>$this->id,
             'start_at'=>$this->start_at,
             'end_at'=>$this->end_at,
-            'price'=>$this->get_price($this),
+            'price'=>$this->get_price($request,$this),
             'discount_amount'=>$this->discount,
         ];
       
     }
-    function get_price($data){
-        $main_price = $data->package->price;
-        $discount_price = $main_price * ($data->discount/ 100);
-        $discount_price = $main_price -$discount_price; 
-        return number_format( $discount_price,2);
+    function get_price($request,$data){
+        if($request->currency == null){
+            $main_price = $data->package->price;
+            $discount_price = $main_price * ($data->discount/ 100);
+            $discount_price = $main_price -$discount_price; 
+            return number_format( $discount_price,2);
+        }else{
+            $main_price = $data->package->price;
+            $discount_price = $main_price * ($data->discount/ 100);
+            $currency = Currency::find($request->currency);
+            $discount_price = ($main_price -$discount_price)* $currency->value_in_dollars; 
+            return number_format( $discount_price,2);
+        }
+
+        
     }
 }
